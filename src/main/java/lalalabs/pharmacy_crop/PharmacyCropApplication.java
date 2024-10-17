c6d5439f5665a156184a338deb460fd9b729ab5c
JooIcksoo <dlrtn751@gmail.com>
2024-10-18 07:29:25 +0900

    feat(root): gradle project setting

---
+++ src/main/java/lalalabs/pharmacy_crop/PharmacyCropApplication.java
@@ -0,0 +1,13 @@
+package lalalabs.pharmacy_crop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyCropApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmacyCropApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lalalabs/pharmacy_crop/PharmacyCropApplicationTests.java
@@ -0,0 +1,16 @@
+package lalalabs.pharmacy_crop;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyCropApplicationTests {
+
+	@Test
+	void always_success() {
+		assertTrue(true);
+	}
+
+}
