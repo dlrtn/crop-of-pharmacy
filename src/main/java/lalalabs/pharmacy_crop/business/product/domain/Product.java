0dfe0a2f18204a8c6d7e73d3282fe5ac9ce32865
JooIcksoo <dlrtn751@gmail.com>
2024-10-25 02:32:11 +0900

    feat(product): 상품 기능 기초 설계 추가

---
+++ src/main/java/lalalabs/pharmacy_crop/business/product/domain/Product.java
@@ -0,0 +1,5 @@
+package lalalabs.pharmacy_crop.business.product.domain;
+
+public class Product {
+
+}
---
+++ src/main/java/lalalabs/pharmacy_crop/business/product/domain/ProductCategory.java
@@ -0,0 +1,9 @@
+package lalalabs.pharmacy_crop.business.product.domain;
+
+public enum ProductCategory {
+    PESTICIDE,
+    FERTILIZER,
+    AGRICULTURAL_MATERIALS,
+    QUARANTINE_MATERIALS,
+    ETC
+}
