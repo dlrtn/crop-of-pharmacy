5351cc7b8e39201760a5ad02144149aab54a3870
JooIcksoo <dlrtn751@gmail.com>
2024-11-06 22:56:18 +0900

    feat(config): Redis 관련 설정 추가
    - OIDC 관련 캐싱용 Redis 추가
    - 그외 캐싱 전략에도 사용 가능

---
+++ src/main/java/lalalabs/pharmacy_crop/common/config/EmbeddedRedisConfig.java
@@ -0,0 +1,38 @@
+package lalalabs.pharmacy_crop.common.config;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import redis.embedded.RedisServer;
+
+@Configuration
+@Slf4j
+public class EmbeddedRedisConfig {
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    private RedisServer redisServer;
+
+    @PostConstruct
+    public void redisServer() {
+
+        redisServer = RedisServer.builder()
+                .port(redisPort)
+                .setting("maxmemory 128M")
+                .build();
+
+        //전 단계에서 생성한 Redis 서버 객체를 실행
+        redisServer.start();
+        log.info("Embedded Redis is started");
+    }
+
+    @PreDestroy
+    public void stopRedis() {
+        if (redisServer != null) {
+            redisServer.stop();
+        }
+    }
+}
---
+++ src/main/java/lalalabs/pharmacy_crop/common/config/RedisRepositoryConfig.java
@@ -0,0 +1,28 @@
+package lalalabs.pharmacy_crop.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+@EnableRedisRepositories
+public class RedisRepositoryConfig {
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(redisHost);
+        redisStandaloneConfiguration.setPort(redisPort);
+
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
+}
