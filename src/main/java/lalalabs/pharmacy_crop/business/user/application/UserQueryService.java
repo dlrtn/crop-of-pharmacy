67b9d64dfec0d7af73fb595641f2f52b3b6cbd3f
JooIcksoo <dlrtn751@gmail.com>
2024-11-09 02:02:48 +0900

    refactor(user.infrastructure): User Query Service 추가

---
+++ src/main/java/lalalabs/pharmacy_crop/business/user/application/UserQueryService.java
@@ -0,0 +1,19 @@
+package lalalabs.pharmacy_crop.business.user.application;
+
+import lalalabs.pharmacy_crop.business.user.domain.OauthUser;
+import lalalabs.pharmacy_crop.business.user.infrastructure.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserQueryService {
+
+    private final UserRepository userRepository;
+
+    public OauthUser findByUserId(String userId) {
+        return userRepository.findById(userId).orElseGet(() -> {
+            throw new IllegalArgumentException("User not found");
+        });
+    }
+}
