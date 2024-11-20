5749d2c72308d8099fb13a33af91d4488d512dab
JooIcksoo <dlrtn751@gmail.com>
2024-11-20 10:42:50 +0900

    feat(announcement): 공지사항 DB 엔티티 추가

--- src/main/java/lalalabs/pharmacy_crop/business/post/domain/Announcement.java
+++ src/main/java/lalalabs/pharmacy_crop/business/post/domain/Announcement.java
@@ -1,6 +1,30 @@
 package lalalabs.pharmacy_crop.business.post.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lalalabs.pharmacy_crop.common.time.BaseTimeEntity;
+
 // 공지사항 도메인
-public class Announcement extends Post {
+@Entity
+public class Announcement extends BaseTimeEntity {
+
+    @Id
+    private Long id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String content;
+
+    @Column
+    private String writer;
+
+    @Column
+    private String picture;
+
+    @Column
+    private String link;
 
 }
