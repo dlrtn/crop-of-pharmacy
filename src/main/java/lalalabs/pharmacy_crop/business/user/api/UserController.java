550f195c2186023232266c416776af549f3091af
JooIcksoo <dlrtn751@gmail.com>
2024-11-25 20:51:50 +0900

    feat(user): 사용자 닉네임 수정 기능 추가

--- src/main/java/lalalabs/pharmacy_crop/business/user/api/UserController.java
+++ src/main/java/lalalabs/pharmacy_crop/business/user/api/UserController.java
@@ -1,11 +1,32 @@
 package lalalabs.pharmacy_crop.business.user.api;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lalalabs.pharmacy_crop.business.user.application.UserCommandService;
+import lalalabs.pharmacy_crop.business.user.domain.OauthUserDetails;
+import lalalabs.pharmacy_crop.common.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "User", description = "사용자의 정보 변경, 관리를 수행합니다.")
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/users")
 public class UserController {
 
+    private final UserCommandService userCommandService;
+
+    @Operation(summary = "닉네임 변경", description = "사용자의 닉네임을 변경합니다.")
+    @PatchMapping("/nickname")
+    public ResponseEntity<ApiResponse> changeNickname(@AuthenticationPrincipal OauthUserDetails oauthUser,
+                                                      @RequestBody String nickname) {
+        userCommandService.updateUserNickname(oauthUser.getUser(), nickname);
+
+        return ResponseEntity.ok().build();
+    }
 }
--- src/main/java/lalalabs/pharmacy_crop/business/user/application/UserCommandService.java
+++ src/main/java/lalalabs/pharmacy_crop/business/user/application/UserCommandService.java
@@ -1,12 +1,16 @@
 package lalalabs.pharmacy_crop.business.user.application;
 
+import jakarta.transaction.Transactional;
+import lalalabs.pharmacy_crop.business.user.domain.OauthUser;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserCommandService {
 
-    public void createUser() {
-
+    @Transactional
+    public void updateUserNickname(OauthUser user, String nickname) {
+        user.updateNickname(nickname);
     }
-
 }
