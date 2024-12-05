bf3bedf3df060df5c5e141d14095073088e2d7d4
JooIcksoo <dlrtn751@gmail.com>
2024-12-05 23:31:28 +0900

    fix(exception): Exception Handler 수정

--- src/main/java/lalalabs/pharmacy_crop/common/handler/CustomExceptionHandler.java
+++ src/main/java/lalalabs/pharmacy_crop/common/handler/CustomExceptionHandler.java
@@ -6,13 +6,21 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 
 @Slf4j
 @RestControllerAdvice
 public class CustomExceptionHandler {
 
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<ApiResponse> handleMissingParamsException(MissingServletRequestParameterException e) {
+        log.warn("Missing Request Parameter: {}", e.getParameterName());
+        return ResponseEntity.status(400).body(ClientErrorResponse.of("Missing parameter: " + e.getParameterName()));
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse> handleUnexpectedException(Exception e) {
+        log.error("Unexpected Exception", e);
         return ResponseEntity.status(500).body(ClientErrorResponse.of(e));
     }
 }
