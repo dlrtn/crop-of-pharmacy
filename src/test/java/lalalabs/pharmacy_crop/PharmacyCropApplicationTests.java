cb69c30da1807bfe57df449d61ebff9a93cdad1a
JooIcksoo <dlrtn751@gmail.com>
2024-12-11 17:36:13 +0900

    feat(test): 테스트 수정

--- src/test/java/lalalabs/pharmacy_crop/PharmacyCropApplicationTests.java
+++ src/test/java/lalalabs/pharmacy_crop/PharmacyCropApplicationTests.java
@@ -3,14 +3,11 @@ package lalalabs.pharmacy_crop;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class PharmacyCropApplicationTests {
 
-	@Test
-	void always_success() {
-		assertTrue(true);
-	}
-
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
 }
