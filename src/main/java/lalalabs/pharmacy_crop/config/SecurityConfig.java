53a0d7b45472f30198989ed4064af04071e89b93
JooIcksoo <dlrtn751@gmail.com>
2025-02-13 11:15:05 +0900

    feat(config): Cors 포트 수정

--- src/main/java/lalalabs/pharmacy_crop/config/SecurityConfig.java
+++ src/main/java/lalalabs/pharmacy_crop/config/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://1.234.83.196/"));
+        configuration.setAllowedOrigins(List.of("http://1.234.83.196/", "http://localhost:3000"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
