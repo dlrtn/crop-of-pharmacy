3f1ff1c8ee3564fd999fa5ee1d30b73ba2852adf
JooIcksoo <dlrtn751@gmail.com>
2025-02-20 01:03:47 +0900

    feat(product): 제품 기능 수정
    - 사진 기능 수정

--- src/main/java/lalalabs/pharmacy_crop/business/product/api/dto/ProductSummaryDto.java
+++ src/main/java/lalalabs/pharmacy_crop/business/product/api/dto/ProductSummaryDto.java
@@ -22,6 +22,8 @@ public class ProductSummaryDto {
 
     private Integer box;
 
+    private String productImage;
+
     public static ProductSummaryDto from(Product product) {
         return ProductSummaryDto.builder()
                 .productId(product.getProductCode())
@@ -31,6 +33,7 @@ public class ProductSummaryDto {
                 .company(product.getCompany())
                 .unit(product.getVolume())
                 .box(product.getQuantityPerBox() == null ? 1 : product.getQuantityPerBox())
+                .productImage("http://1.234.83.196:8080" + product.getPicture().substring(4))
                 .build();
     }
 }
