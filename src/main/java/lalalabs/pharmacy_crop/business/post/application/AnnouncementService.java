32c437773a3dfd5b41aa289115ad3e31cc7ac913
JooIcksoo <dlrtn751@gmail.com>
2025-05-09 16:43:02 +0900

    fix(announcement): 공지사항 작성 시, 알림 활성화

--- src/main/java/lalalabs/pharmacy_crop/business/post/application/AnnouncementService.java
+++ src/main/java/lalalabs/pharmacy_crop/business/post/application/AnnouncementService.java
@@ -42,7 +42,7 @@ public class AnnouncementService {
             announcement.updatePicturePath(localFileUploader.upload(file, DirectoryType.ANNOUNCEMENT));
         }
 
-        // pushNotificationSender.sendAnnouncementPushNotification();
+        pushNotificationSender.sendAnnouncementPushNotification();
 
         Announcement newAnnouncement = announcementRepository.save(announcement);
 
